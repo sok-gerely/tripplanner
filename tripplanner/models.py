5e10e229f27d09355a780e2c7cb8a96d2afb957d
kovacsge11 <kovacsge11@users.noreply.github.com>
2019-11-07 18:53:01 +0100

    Corrected mistakes, created first migration

--- tripplanner/models.py
+++ tripplanner/models.py
@@ -1,28 +1,30 @@
 from django.db import models
 
 class Station(models.Model):
-    lines = models.ManyToManyField(Line)
     xpos = models.FloatField(default=0.0)
-    ypos = models.FloatField(default=0.0)    
+    ypos = models.FloatField(default=0.0)
+
 
 class Line(models.Model):
     #is it sortable this way
     stations = models.ManyToManyField(
                             Station,
-                            through=StationOrder)
-
+                            through='StationOrder')
+                            
 class StationOrder(models.Model):
+    station = models.ForeignKey(Station, on_delete=models.CASCADE)
+    line = models.ForeignKey(Line, on_delete=models.CASCADE)
     order_num = models.IntegerField()
 
 class Service(models.Model):
     fee = models.IntegerField(default=1)
     line = models.ForeignKey(Line, on_delete=models.CASCADE)
-    timetable = models.OneToOneField(Timetable, on_delete=models.CASCADE)
-
-class Timetable(models.Model):
     
 
-class TimeTableData(models.Model):
+class Timetable(models.Model):
+    service = models.OneToOneField(Service, on_delete=models.CASCADE)
+
+class TimetableData(models.Model):
     timetable = models.ForeignKey(Timetable, on_delete=models.CASCADE)
     #is the order of stations handled this way?
     station_order = models.ForeignKey(StationOrder(), on_delete=models.CASCADE) #is cascade necessary here?
