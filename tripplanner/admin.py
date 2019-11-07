79d3feef1f91fd5e52375c512e1edf9b52f6bacc
kovacsge11 <kovacsge11@users.noreply.github.com>
2019-11-07 18:13:53 +0100

    Added models to admin site and tripplanner app to tripplannersite

--- tripplanner/admin.py
+++ tripplanner/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Station,Line,StationOrder,Service,Timetable,TimetableData,Delay
+
+admin.site.register(Station)
+admin.site.register(Line)
+admin.site.register(StationOrder)
+admin.site.register(Service)
+admin.site.register(Timetable))
+admin.site.register(TimetableData)
+admin.site.register(Delay)
--- tripplannersite/settings.py
+++ tripplannersite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tripplanner.apps.TripplannerConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
