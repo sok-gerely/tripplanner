8a90b2c137ad6bc302869fbfb3e11f76aead2f48
Ákos Gángoly <gangoly96@gmail.com>
2019-11-17 21:09:42 +0100

    all 3 searching modes are working

--- tripplanner/planning_alg.py
+++ tripplanner/planning_alg.py
@@ -16,7 +16,9 @@ def plan():
     destination_station = Station.objects.get(name=destination_name)
 
     dijkstra = Dijkstra(get_neighbors_distance)
-    dist, prev, time_leave, time_arrive, line = dijkstra(start_station.id, start_time)
+    # dijkstra = Dijkstra(get_neighbors_cost)
+    # dijkstra = Dijkstra(get_neighbors_time)
+    dist, prev, time_leave_prev, time_arrive, line_prev = dijkstra(start_station.id, start_time)
     route = [destination_station.id]
     while True:
         u = prev[route[-1]]
@@ -26,15 +28,15 @@ def plan():
     route.reverse()
 
     time_arrive[start_station.id] = None
-    time_leave[destination_station.id] = None
-    line[destination_station.id] = None
+    time_leave_prev[start_station.id] = None
+    line_prev[start_station.id] = None
 
-    return [(time_arrive[r], Station.objects.values_list('name', flat=True).get(id=r), time_leave[r], line[r]) for r in
+    return [(time_arrive[r], Station.objects.values_list('name', flat=True).get(id=r), time_leave_prev[r], line_prev[r]) for r in
             route]
 
 
 NeighbourResult = namedtuple('NeighbourResult',
-                             ['v', 'distance', 'line_name', 'u_leave_time', 'v_arrive_time'])
+                             ['v', 'distance', 'line_name', 'u_leave_time', 'v_arrive_time'])  # TODO fee
 
 
 class Dijkstra:
@@ -46,9 +48,9 @@ class Dijkstra:
         Q = list(Station.objects.values_list('id', flat=True))
         dist = defaultdict(lambda: inf)
         time_arrive = defaultdict(lambda: inf)
-        time_leave = defaultdict(lambda: inf)
+        time_leave_prev = defaultdict(lambda: inf)
         prev = defaultdict(None)
-        line = defaultdict(None)
+        line_prev = defaultdict(None)
         dist[start_station] = 0
         time_arrive[start_station] = start_time
         while len(Q) != 0:
@@ -64,9 +66,9 @@ class Dijkstra:
                     dist[res.v] = alt
                     prev[res.v] = u
                     time_arrive[res.v] = res.v_arrive_time
-                    time_leave[u] = res.u_leave_time
-                    line[u] = res.line_name
-        return dist, prev, time_leave, time_arrive, line
+                    time_leave_prev[res.v] = res.u_leave_time
+                    line_prev[res.v] = res.line_name
+        return dist, prev, time_leave_prev, time_arrive, line_prev
 
     def get_Q_min_dist(self, Q, dist):
         min_d = inf
@@ -94,7 +96,7 @@ def get_neighbors_distance(u, t):
 
 def get_neighbors_cost(u, t):
     station_service = StationOrder.objects.filter(station_from=u).values_list(
-        'station_to', 'fee', 'line__name', 'line__service')
+        'station_to', 'line__service__fee', 'line__name', 'line__service')
 
     res = []
     for v, distance, line__name, service in station_service:
@@ -107,5 +109,20 @@ def get_neighbors_cost(u, t):
 
 
 def get_neighbors_time(u, t):
-    return StationOrder.objects.filter(station_from=u, line__service__timetabledata__date_time__gt=t).values_list(
-        'station_to', 'line__service__fee', 'line__name', 'line__service__timetabledata__date_time')
+    station_service = StationOrder.objects.filter(station_from=u).values_list(
+        'station_to', 'line__name', 'line__service')
+
+    res = []
+    for v, line__name, service in station_service:
+        v_arrive_time = TimetableData.objects.get(service=service, station=v).date_time
+        u_leave_time = TimetableData.objects.get(service=service, station=u).date_time
+        if u_leave_time > t:
+            seconds = int((time2datetime(v_arrive_time) - time2datetime(t)).total_seconds())
+            res.append(NeighbourResult(v=v, distance=seconds,
+                                       line_name=line__name,
+                                       u_leave_time=u_leave_time, v_arrive_time=v_arrive_time))
+    return res
+
+
+def time2datetime(t: datetime.time):
+    return datetime.datetime(1, 1, 1, t.hour, t.minute, t.second, t.microsecond)
--- tripplanner/views.py
+++ tripplanner/views.py
@@ -5,4 +5,4 @@ from tripplanner.planning_alg import plan
 
 def index(request):
     gen = plan()
-    return HttpResponse('<br>'.join(f'{a} --> {t}, {s}, {l}' for a, s, t, l in gen))
+    return HttpResponse('<br>'.join(f'{l} {t} --> {a}, {s}' for a, s, t, l in gen))
