d413444843bd9ea519a533fc79288916abd93523
Ákos Gángoly <gangoly96@gmail.com>
2019-11-26 20:01:25 +0100

    more clean up

--- tripplanner/planning_alg.py
+++ tripplanner/planning_alg.py
@@ -1,10 +1,14 @@
 from enum import Enum
-from typing import List
+from typing import List, Tuple, Iterator
 
 import pandas as pd
 
 from tripplanner.bi.dijkstra import Dijkstra, WeightArgs, RouteInfo
 from tripplanner.models import *
+from tripplanner import constants
+
+TransposeMiddle = Iterator[Tuple[datetime.datetime, Station]]
+TransposeEndpoint = List[List]
 
 
 class NoRouteExists(Exception):
@@ -36,8 +40,20 @@ class PlanningMode(Enum):
         return self.value
 
 
+def get_weight_distance(args: WeightArgs):
+    return args.distance
+
+
+def get_weight_cost(args: WeightArgs):
+    return args.fee
+
+
+def get_weight_time(args: WeightArgs):
+    return (args.v_arrive_time - args.t).total_seconds()
+
+
 def plan(planning_mode: PlanningMode, start_time: datetime.datetime, start_station: Station,
-         destination_station: Station) -> pd.DataFrame:
+         destination_station: Station) -> Tuple[TransposeEndpoint, List[TransposeMiddle], float, float]:
     if start_station == destination_station:
         raise StationsAreTheSame()
 
@@ -47,7 +63,11 @@ def plan(planning_mode: PlanningMode, start_time: datetime.datetime, start_stati
         raise NoRouteExists()
 
     df = __routeinfos2df(start_station, destination_station, info)
-    return df
+    endpoints_df = __split_df_to_middle_endpoints(df)
+    middles_list = __get_list_zip_of_middle(df, endpoints_df)
+    total_cost = endpoints_df['Fee'].sum(),
+    total_time = endpoints_df['End time'].iloc[-1] - endpoints_df.at[0, 'Start time']
+    return _endpointsdf2list(endpoints_df), middles_list, total_cost[0], total_time
 
 
 def __routeinfos2df(start_station: Station, destination_station: Station, infos: List[RouteInfo]) -> pd.DataFrame:
@@ -67,13 +87,52 @@ def __routeinfos2df(start_station: Station, destination_station: Station, infos:
     return df
 
 
-def get_weight_distance(args: WeightArgs):
-    return args.distance
+def __split_df_to_middle_endpoints(df: pd.DataFrame) -> pd.DataFrame:
+    df['prev line'] = df['Line'].shift(1)
+    df['Middle Station'] = df['prev line'] == df['Line']
+    ends_df = df[~df['Middle Station']]
+    endpoints_df = __get_endpoints_df(ends_df)
+    return endpoints_df
 
 
-def get_weight_cost(args: WeightArgs):
-    return args.fee
+def __get_endpoints_df(ends_df: pd.DataFrame) -> pd.DataFrame:
+    endpoints_df = pd.DataFrame({'Start station': ends_df['Station'][:-1],
+                                 'End station': ends_df['Station'].shift(-1)[:-1],
+                                 'Start time': ends_df['Leave time'][:-1],
+                                 'End time': ends_df['Arrive time'].shift(-1)[:-1],
+                                 'Start index': ends_df.index[:-1],
+                                 'End index': ends_df.index[1:],
+                                 'Fee': ends_df['Fee'][:-1],
+                                 'Line': ends_df['Line'][:-1]})
+    endpoints_df['Travel time'] = (endpoints_df['End time'] - endpoints_df['Start time'])  # .apply(
+    # lambda x: f'{x.hour}')##.time().strptime(f'{time_str}'))
+    return endpoints_df
 
 
-def get_weight_time(args: WeightArgs):
-    return (args.v_arrive_time - args.t).total_seconds()
+def __get_list_zip_of_middle(df: pd.DataFrame, endpoints_df: pd.DataFrame) -> List[TransposeMiddle]:
+    middles_df = df[df['Middle Station']]
+    middles_list = []
+    for i, row in endpoints_df[['Start index', 'End index']].iterrows():
+        df = middles_df[(row['Start index'] <= middles_df.index) & (middles_df.index <= row['End index'])]
+        endpoints_df.at[i, 'Fee'] *= df.shape[0] + 1
+        middles_list.append(__middlesdf2zip(df))
+    return middles_list
+
+
+def __middlesdf2zip(df: pd.DataFrame) -> TransposeMiddle:
+    return zip(df['Leave time'].apply(format_datetime).to_list(),
+               df['Station'].to_list())
+
+
+def _endpointsdf2list(df: pd.DataFrame) -> TransposeEndpoint:
+    return [df['Start time'].apply(format_datetime).to_list(),
+            df['End time'].apply(format_datetime).to_list(),
+            df['Start station'].to_list(),
+            df['End station'].to_list(),
+            df['Fee'].to_list(),
+            df['Line'].to_list(),
+            df['Travel time'].to_list()]
+
+
+def format_datetime(t):
+    return t.strftime(f'{constants.time_format} {constants.date_fromat}')
--- tripplanner/views.py
+++ tripplanner/views.py
@@ -1,7 +1,5 @@
-import pandas as pd
 import json
 
-from django.http import HttpResponse, Http404
 import datetime
 
 from django.shortcuts import render, redirect, get_object_or_404
@@ -22,56 +20,17 @@ def result(request, planning_mode_str: str, date_str: str, time_str: str, statio
     start_datetime = datetime.datetime.strptime(f'{date_str} {time_str}',
                                                 f'{constants.date_fromat} {constants.time_format}')
     try:
-        df = plan(planning_mode, start_datetime, start_station, destination_station)
-        # ['Arrive time', 'Leave time', 'Station', 'Line', 'Fee']
-        df['prev line'] = df['Line'].shift(1)
-        df['Middle Station'] = df['prev line'] == df['Line']
-        ends_df = df[~df['Middle Station']]
-        middles_df = df[df['Middle Station']]
-        endpoints_df = pd.DataFrame({'Start station': ends_df['Station'][:-1],
-                                     'End station': ends_df['Station'].shift(-1)[:-1],
-                                     'Start time': ends_df['Leave time'][:-1],
-                                     'End time': ends_df['Arrive time'].shift(-1)[:-1],
-                                     'Start index': ends_df.index[:-1],
-                                     'End index': ends_df.index[1:],
-                                     'Fee': ends_df['Fee'][:-1],
-                                     'Line': ends_df['Line'][:-1]})
-        endpoints_df['Travel time'] = (endpoints_df['End time'] - endpoints_df['Start time'])  # .apply(
-        # lambda x: f'{x.hour}')##.time().strptime(f'{time_str}'))
-        middles_zips = []
-        for i, row in endpoints_df[['Start index', 'End index']].iterrows():
-            df = middles_df[(row['Start index'] <= middles_df.index) & (middles_df.index <= row['End index'])]
-            endpoints_df.at[i, 'Fee'] *= df.shape[0] + 1
-            middles_zips.append(middlesdf2list(df))
+        endpoints_list, middles_list, total_cost, total_time = plan(planning_mode, start_datetime, start_station,
+                                                                    destination_station)
+        data = endpoints_list
+        data.append(middles_list)
+        return render(request, 'tripplanner/result.html',
+                      {'data': zip(*data), 'total_cost': total_cost,
+                       'total_time': total_time})
     except NoRouteExists:
         return render(request, 'tripplanner/error.html', {'message': "Route doesn't exist!"})
     except StationsAreTheSame:
         return render(request, 'tripplanner/error.html', {'message': "The start and destination can't be the same!"})
-
-    data = endpointsdf2list(endpoints_df)
-    data.append(middles_zips)
-    return render(request, 'tripplanner/result.html',
-                  {'data': zip(*data), 'total_cost': endpoints_df['Fee'].sum(),
-                   'total_time': endpoints_df['End time'].iloc[-1] - endpoints_df.at[0, 'Start time']})
-
-
-def format_datetime(t):
-    return t.strftime(f'{constants.time_format} {constants.date_fromat}')
-
-
-def middlesdf2list(df):
-    return zip(df['Leave time'].apply(format_datetime).to_list(),
-               df['Station'].to_list())
-
-
-def endpointsdf2list(df):
-    return [df['Start time'].apply(format_datetime).to_list(),
-            df['End time'].apply(format_datetime).to_list(),
-            df['Start station'].to_list(),
-            df['End station'].to_list(),
-            df['Fee'].to_list(),
-            df['Line'].to_list(),
-            df['Travel time'].to_list()]
 
 
 def index(request):
