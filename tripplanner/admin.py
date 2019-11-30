411f126b6e8c7f174dbbfe1efe5222257babe0e3
Ákos Gángoly <gangoly96@gmail.com>
2019-12-01 00:10:50 +0100

    autocomplete

--- tripplanner/admin.py
+++ tripplanner/admin.py
@@ -1,4 +1,5 @@
-import modelclone
+from django.db.models import F
+from django.forms import modelform_factory
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
 from django.forms.models import ModelForm, BaseInlineFormSet
@@ -21,6 +22,38 @@ class AlwaysChangedModelForm(ModelForm):
 
 class StationOrderInlineFromSet(BaseInlineFormSet):
 
+    def __init__(self, data=None, files=None, instance=None, save_as_new=False, prefix=None, queryset=None, **kwargs):
+        if len(queryset) > 0:
+            last_insatnce = queryset[len(queryset) - 1]
+            last_station_id = last_insatnce.station_to_id
+            StationOrder(station_from_id=last_station_id, station_to_id=last_station_id,
+                         line_id=last_insatnce.line_id).save()
+        super().__init__(data, files, instance, save_as_new, prefix, queryset, **kwargs)
+
+    def __del__(self):
+        StationOrder.objects.filter(station_from_id=F('station_to_id')).delete()
+
+    # def get_queryset(self):
+    #     queryset = super().get_queryset()
+    #     last_insatnce = queryset[len(queryset) - 1]
+    #     last_station_id = last_insatnce.station_to_id
+    #     StationOrder(station_from_id=last_station_id, station_to_id=last_station_id, line_id=last_insatnce.line_id).save()
+    #     return super().get_queryset()
+
+    # def __init__(self, data=None, files=None, instance=None, save_as_new=False, prefix=None, queryset=None, **kwargs):
+    #     super().__init__(data, files, instance, save_as_new, prefix, queryset, **kwargs)
+    #     self.extra_forms[0].instance.station_from_id = self.forms[-1].instance.station_to_id
+    #     # self.extra_forms[0].instance.distance = self.forms[-1].instance.distance
+    #     # self.extra_forms[0].fields['distance'].initial = 5 #self.forms[-1].instance.distance
+    #     self.forms[-1].fields['station_from'].initial = self.forms[-1].instance.station_to_id
+    #     self.forms[-1].fields['station_from'].show_hidden_initila = True
+    #     # StationOrderForm = modelform_factory(StationOrder, exclude=('station_to',))
+    #     # self.forms.append(
+    #     #     StationOrderForm(instance=StationOrder(station_from_id=self.forms[-1].instance.station_to_id)))
+    #     # for form in self.forms:
+    #     #     print(form)
+    #
+
     def save(self, commit=True):
         instances = super().save(False)
         for next, current in zip(instances[1:], instances):
@@ -31,18 +64,15 @@ class StationOrderInlineFromSet(BaseInlineFormSet):
             instances.pop()
         return instances
 
-    def clean(self):
-        for form in self.forms:
-            print(form)
-
 
 class StationOrderInline(admin.TabularInline):
+
+    autocomplete_fields = ['station_from']
     model = StationOrder
     fk_name = "line"
     exclude = ('station_to',)
     extra = 0
     formset = StationOrderInlineFromSet
-    # form = NameForm
 
 
 class ServiceInline(admin.TabularInline):
@@ -69,12 +99,22 @@ class TimetableDataInline(admin.TabularInline):
 
 
 class LineAdmin(admin.ModelAdmin):
+    save_as = True
+
+    # def __init__(self, model, admin_site):
+    #
+    #     # queryset = StationOrder.objects.filter(line_id=)
+    #     # last_insatnce = queryset[len(queryset) - 1]
+    #     # last_station_id = last_insatnce.station_to_id
+    #     # StationOrder(station_from_id=last_station_id, station_to_id=last_station_id, line_id=last_insatnce.line_id).save()
+    #     # return super().get_queryset()
+    #     super().__init__(model, admin_site)
 
     def get_inline_instances(self, request, obj=None):
         inlines_edit = [ServiceInline, ]
         inlines_create = inlines_edit + [StationOrderInline, ]
         if obj:
-            inlines = inlines_edit
+            inlines = inlines_create  # inlines_edit
         else:
             inlines = inlines_create
         return [inline(self.model, self.admin_site) for inline in inlines]
@@ -101,6 +141,7 @@ class TimetableDataAdmin(admin.ModelAdmin):
 
 
 class StationAdmin(admin.ModelAdmin):
+    search_fields = ['name']
     list_display = ('name',)
 
 
